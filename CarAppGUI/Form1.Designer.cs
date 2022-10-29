bcccfae8df2376d9aaccd72fa048d20a372f54d5
cdog2897 <caleb.wolin@gmail.com>
2022-10-29 01:28:04 -0700

    Add project files.

---
+++ CarAppGUI/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace CarAppGUI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtCondition = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstInventory = new System.Windows.Forms.ListBox();
+            this.Checkout = new System.Windows.Forms.GroupBox();
+            this.lstCart = new System.Windows.Forms.ListBox();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.btnCheckout = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblException = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.Checkout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCreate);
+            this.groupBox1.Controls.Add(this.txtColor);
+            this.groupBox1.Controls.Add(this.txtCondition);
+            this.groupBox1.Controls.Add(this.txtPrice);
+            this.groupBox1.Controls.Add(this.txtModel);
+            this.groupBox1.Controls.Add(this.txtMake);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 472);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Create a Car";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(153, 374);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(192, 45);
+            this.btnCreate.TabIndex = 11;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(153, 301);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(192, 31);
+            this.txtColor.TabIndex = 10;
+            // 
+            // txtCondition
+            // 
+            this.txtCondition.Location = new System.Drawing.Point(153, 246);
+            this.txtCondition.Name = "txtCondition";
+            this.txtCondition.Size = new System.Drawing.Size(192, 31);
+            this.txtCondition.TabIndex = 9;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(153, 188);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(192, 31);
+            this.txtPrice.TabIndex = 8;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(153, 132);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(192, 31);
+            this.txtModel.TabIndex = 7;
+            // 
+            // txtMake
+            // 
+            this.txtMake.Location = new System.Drawing.Point(153, 80);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(192, 31);
+            this.txtMake.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Color";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Condition";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Model";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Make";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstInventory);
+            this.groupBox2.Location = new System.Drawing.Point(459, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(388, 578);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Car Inventory";
+            // 
+            // lstInventory
+            // 
+            this.lstInventory.FormattingEnabled = true;
+            this.lstInventory.ItemHeight = 25;
+            this.lstInventory.Location = new System.Drawing.Point(6, 60);
+            this.lstInventory.Name = "lstInventory";
+            this.lstInventory.Size = new System.Drawing.Size(382, 504);
+            this.lstInventory.TabIndex = 0;
+            // 
+            // Checkout
+            // 
+            this.Checkout.Controls.Add(this.lstCart);
+            this.Checkout.Location = new System.Drawing.Point(1057, 29);
+            this.Checkout.Name = "Checkout";
+            this.Checkout.Size = new System.Drawing.Size(331, 638);
+            this.Checkout.TabIndex = 2;
+            this.Checkout.TabStop = false;
+            this.Checkout.Text = "Checkout";
+            // 
+            // lstCart
+            // 
+            this.lstCart.FormattingEnabled = true;
+            this.lstCart.ItemHeight = 25;
+            this.lstCart.Location = new System.Drawing.Point(6, 36);
+            this.lstCart.Name = "lstCart";
+            this.lstCart.Size = new System.Drawing.Size(319, 579);
+            this.lstCart.TabIndex = 1;
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.Location = new System.Drawing.Point(866, 403);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(169, 45);
+            this.btnAddToCart.TabIndex = 3;
+            this.btnAddToCart.Text = "Add to Cart ->";
+            this.btnAddToCart.UseVisualStyleBackColor = true;
+            this.btnAddToCart.Click += new System.EventHandler(this.btnAddToCart_Click);
+            // 
+            // btnCheckout
+            // 
+            this.btnCheckout.Location = new System.Drawing.Point(1150, 685);
+            this.btnCheckout.Name = "btnCheckout";
+            this.btnCheckout.Size = new System.Drawing.Size(169, 45);
+            this.btnCheckout.TabIndex = 4;
+            this.btnCheckout.Text = "Checkout";
+            this.btnCheckout.UseVisualStyleBackColor = true;
+            this.btnCheckout.Click += new System.EventHandler(this.btnCheckout_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(1195, 756);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 25);
+            this.lblTotal.TabIndex = 5;
+            // 
+            // lblException
+            // 
+            this.lblException.AutoSize = true;
+            this.lblException.Location = new System.Drawing.Point(30, 542);
+            this.lblException.Name = "lblException";
+            this.lblException.Size = new System.Drawing.Size(355, 25);
+            this.lblException.TabIndex = 12;
+            this.lblException.Text = "That is not an acceptable data type!";
+            this.lblException.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1536, 904);
+            this.Controls.Add(this.lblException);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnCheckout);
+            this.Controls.Add(this.btnAddToCart);
+            this.Controls.Add(this.Checkout);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.Checkout.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtCondition;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lstInventory;
+        private System.Windows.Forms.GroupBox Checkout;
+        private System.Windows.Forms.ListBox lstCart;
+        private System.Windows.Forms.Button btnAddToCart;
+        private System.Windows.Forms.Button btnCheckout;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblException;
+    }
+}
+
---
+++ CarAppGUI/Form1.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Caleb Wolin
+ * CST-250
+ * 9/16/22
+ */
+using CarClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarAppGUI
+{
+    public partial class Main : Form
+    {
+        // create a new store instance
+        Store myStore = new Store();
+        // binding source for linking arraylist to list 
+        BindingSource carInventoryBindingSource = new BindingSource();
+        BindingSource cartBindingSource = new BindingSource();
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            // check for exceptions before adding to list
+            try
+            {
+                int price = int.Parse(txtPrice.Text);
+                int condition = int.Parse(txtCondition.Text);
+
+                // create new car and add to list
+                Car c = new Car(txtMake.Text, txtModel.Text, price, condition, txtColor.Text);
+
+                myStore.CarList.Add(c);
+                // reset values
+                carInventoryBindingSource.ResetBindings(false);
+
+                txtMake.Text = null;
+                txtModel.Text = null;
+                txtPrice.Text = null;
+                txtCondition.Text = null;
+                txtColor.Text = null;
+
+                lblException.Visible = false;
+            }
+            catch (Exception except)
+            {
+                lblException.Visible = true;
+                txtMake.Text = null;
+                txtModel.Text = null;
+                txtPrice.Text = null;
+                txtCondition.Text = null;
+                txtColor.Text = null;
+            }
+
+        }
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            // add to cart list
+            Car selected = (Car) lstInventory.SelectedItem;
+            myStore.ShoppingList.Add(selected);
+            cartBindingSource.ResetBindings(false);
+        }
+        private void btnCheckout_Click(object sender, EventArgs e)
+        {
+            // calculate total
+            decimal total = myStore.Checkout();
+            lblTotal.Text = "$" + total.ToString();
+            cartBindingSource.ResetBindings(false);
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            carInventoryBindingSource.DataSource = myStore.CarList;
+            cartBindingSource.DataSource = myStore.ShoppingList;
+            lstInventory.DataSource = carInventoryBindingSource;
+            lstInventory.DisplayMember = ToString();
+            lstCart.DataSource = cartBindingSource;
+            lstCart.DisplayMember = ToString();
+        }
+    }
+}
